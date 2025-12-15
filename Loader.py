68183929ed691c071f3e2504b17c947d592f59d8
Cool Dude <dahronirvingjr@gmail.com>
2025-12-15 16:50:45 -0700

    Update Loader.py

--- Loader.py
+++ Loader.py
@@ -2,7 +2,7 @@ local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/Slen
 
 -- This was coded on mobile
 
-local Window = OrionLib:MakeWindow({Name = "Script Loader", HidePremium = false, SaveConfig = true, IntroText="Script Loader",ConfigFolder = "SLC"})
+local Window = OrionLib:MakeWindow({Name = "Script Loader", HidePremium = false, SaveConfig = true, IntroText="Script Loader",Icon="rbxassetid://16481845020",ConfigFolder = "SLC"})
 
 local scripttab = Window:MakeTab({
 	Name = "scripts",
@@ -12,6 +12,19 @@ local scripttab = Window:MakeTab({
 local settingstab = Window:MakeTab({
 	Name = "Settings",
 	Icon = "rbxassetid://6918434507"
+})
+
+local keyless = scripttab:AddSection({
+	Name = "Keyless Scripts"
+})
+
+local key = scripttab:AddSection({
+	Name = "Key System Scripts"
+})
+
+local gamestab = Window:MakeTab({
+	Name = "Game Scripts",
+	Icon = "rbxassetid://238317630"
 })
 
 local function notif(name,ctx,time,img)
@@ -24,37 +37,46 @@ local function notif(name,ctx,time,img)
 	})
 end
 
-notif("Loaded","script loader has been loaded","5","1")
+notif("Loaded","script loader has been loaded","rbxassetid://11604833061",1)
 
-scripttab:AddButton({
+keyless:AddButton({
 	Name = "Solara Hub",
 	
 	Callback = function()
 		loadstring(game:HttpGet('https://raw.githubusercontent.com/samuraa1/Solara-Hub/refs/heads/main/SH.lua'))()
-		notif("Script Loaded","Solara Hub has been loaded","123",5)
+		notif("Script Loaded","Solara Hub has been loaded","rbxassetid://11604833061",5)
 	
 	end
 })
 
-scripttab:AddButton({
+key:AddButton({
 	Name = "Monke Hub",
 	
 	Callback = function()
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/Monkescripter589/monke-hub-/refs/heads/main/monke%20hub"))()
-		notif("Script Loaded","Monke Hub has been loaded","123",5)
+		notif("Script Loaded","Monke Hub has been loaded","rbxassetid://11604833061",5)
 	
 	end
 })
 
-scripttab:AddButton({
+keyless:AddButton({
 	Name = "Internal",
 	
 	Callback = function()
 		loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/MappleGaming208/Magic/main/Scripts/lua/zzerexx/Internal_UI.lua"))()
 		task.wait(3)
 		print("Internal Ui Loaded! Press insert to open or press the internal ui button to open!")
-		notif("Script Loaded","Internal UI has been loaded","123",5)
+		notif("Script Loaded","Internal UI has been loaded","rbxassetid://11604833061",5)
 
+	end
+})
+
+
+key:AddButton({
+	Name = "Luna Hub (Key is UnoXLuna)",
+	Callback = function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/UnoXLuna/Luna-Hub/refs/heads/main/Script-Loader"))()
+		notif("Script Loaded","Luna Hub has been loaded","rbxassetid://11604833061",5)
 	end
 })
 
@@ -66,8 +88,16 @@ settingstab:AddButton({
 })
 
 
-
-
+local fortlinespt = gamestab:AddSection({
+	Name = "Fortline"
+})
+fortlinespt:AddButton({
+	Name = "Hydra Hub",
+	Callback = function()
+		loadstring(game:HttpGet("https://rawscripts.net/raw/FORTLINE-free-vip-script-24909"))()
+		notif("Script Loaded","Hydra Hub has been loaded","rbxassetid://11604833061",5)
+	end
+})
 
 
 OrionLib:Init()
