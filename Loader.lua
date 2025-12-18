9c082e7cc34c499be9d4ba95abfbd4c76d530319
Cool Dude <dahronirvingjr@gmail.com>
2025-12-17 18:02:34 -0700

    added functions

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,26 @@
+local Players = game:GetService("Players")
+local plr = Players.LocalPlayer
+local lib = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
+local Window = lib:CreateWindow({
+    Title = "Aqua Hub - Custom",
+    Icon = "Shell",
+    Author = "by cooldudeisbetter",
+    Folder = "Aqua Hub"
+
+    User = {
+        Enabled = true,
+        Anonymous = false,
+        Callback = function()
+            print("Not a clickable object")
+        end,
+    }
+})
+
+local home = Window:Tab({
+    Name = "Home",
+    Icon = "house",
+    Locked = false,
+})
+
+home:Select()
