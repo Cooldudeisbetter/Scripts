795346b05c43c249b7ea7d9079e49c4ce457269b
Cool Dude <dahronirvingjr@gmail.com>
2025-12-18 17:32:50 -0700

    testing

--- fpsbypasser.lua
+++ fpsbypasser.lua
@@ -1,19 +1,22 @@
 while true do
-    f = math.random(8000,10000)
+    f = math.random(100,240)
+    m = math.random(1500,3000)
 
     local args = {
         [1] = {
             ["fps"] = f,
-            ["mem"] = 13158.694580078125,
+            ["mem"] = m,
             ["t"] = "metrics",
             ["res"] = Vector2.new(1180, 820),
             ["gfx"] = Enum.SavedQualitySetting.Automatic
         }
+        
     }
 
 
     game:GetService("ReplicatedStorage"):WaitForChild("nya"):FireServer(unpack(args))
+    print(args[1]["t"])
 
-    task.wait(0.5)
+    task.wait(0.25)
 
 end
