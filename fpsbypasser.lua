a9cff55a7cbe70ddf941e98cd1f08426c53839f7
Cool Dude <dahronirvingjr@gmail.com>
2025-12-18 17:21:11 -0700

    added an ungodly amount of frames to the visual stuff

--- fpsbypasser.lua
+++ fpsbypasser.lua
@@ -1,11 +1,17 @@
-local args = {
-    [1] = {
-        ["fps"] = 100.131331337126323,
-        ["mem"] = 1318.694580078125,
-        ["t"] = "metrics",
-        ["res"] = Vector2.new(1180, 820),
-        ["gfx"] = Enum.SavedQualitySetting.Automatic
+while true do
+    local args = {
+        [1] = {
+            ["fps"] = 8363409,
+            ["mem"] = 13158.694580078125,
+            ["t"] = "metrics",
+            ["res"] = Vector2.new(1180, 820),
+            ["gfx"] = Enum.SavedQualitySetting.Automatic
+        }
     }
-}
 
-game:GetService("ReplicatedStorage"):WaitForChild("nya"):FireServer(unpack(args))
+
+    game:GetService("ReplicatedStorage"):WaitForChild("nya"):FireServer(unpack(args))
+
+    task.sleep(0.5)
+
+end
