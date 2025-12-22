1f80283485712ea49807fd13ecbcdeabe115f2eb
Cool Dude <dahronirvingjr@gmail.com>
2025-12-22 11:34:08 -0700

    rework

--- temp.lua
+++ temp.lua
@@ -1,17 +1,20 @@
+local rng = Random.new()  
+
 while true do
-        fps = math.random(10000,15000)
-        mem = math.random(2000,5000)
-        local args = {
-            [1] = {
-                ["res"] = "2400x1000" --[[Vector2]],
-                ["mem"] = mem,
-                ["t"] = "metrics",
-                ["gfx"] = Enum.SavedQualitySetting.Automatic,
-                ["fps"] = fps
-            }
+    local fps = rng:NextInteger(10000, 15000)  
+    local mem = rng:NextInteger(2000, 5000)
+    
+    local args = {
+        [1] = {
+            ["res"] = "2400x1000" --[Vector2]],
+            ["mem"] = mem,
+            ["t"] = "metrics",
+            ["gfx"] = Enum.SavedQualitySetting.Automatic,
+            ["fps"] = fps
         }
-
-        game:GetService("ReplicatedStorage").nya:FireServer(unpack(args))
-
-        task.wait(0.5)
+    }
+    
+    game:GetService("ReplicatedStorage"):WaitForChild("ReplicatedStorage").nya:FireServer(unpack(args))  -- Fixed typo: was "ReplicatedStorage" twice
+    
+    task.wait(0.25)
 end
