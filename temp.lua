d806f808ab1bbbbc5941be20f2604832e365163f
Cool Dude <dahronirvingjr@gmail.com>
2025-12-22 11:29:08 -0700

    fixed line 5

--- temp.lua
+++ temp.lua
@@ -1,11 +1,13 @@
 while true do
+        fps = math.random(10000,15000)
+        mem = math.random(2000,5000)
         local args = {
             [1] = {
                 ["res"] = "2400x1000" --[[Vector2]],
-                ["mem"] = math.random(5000,2000),
+                ["mem"] = mem,
                 ["t"] = "metrics",
                 ["gfx"] = Enum.SavedQualitySetting.Automatic,
-                ["fps"] = math.random(10000,15000)
+                ["fps"] = fps
             }
         }
 
